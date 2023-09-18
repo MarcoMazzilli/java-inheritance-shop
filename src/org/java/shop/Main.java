3669e6f06f027b7d8e57245787bab0a75f0d05a3
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-09-18 15:04:17 +0200

    [Fisrts Commit]Basic scaffolding product class

---
+++ src/org/java/shop/Main.java
@@ -0,0 +1,12 @@
+package org.java.shop;
+
+public class Main {
+	
+	public static void main(String[] args) {
+
+		Prodotto p1 = new Prodotto("Iphone 15","Apple", 799.99f);
+		System.out.println(p1);
+		
+	}
+	
+}
---
+++ src/org/java/shop/Prodotto.java
@@ -0,0 +1,73 @@
+package org.java.shop;
+
+public class Prodotto {
+	
+	private String codice;
+	private String nome;
+	private String marca;
+	private float prezzo;
+	private final int IVA = 22; 
+	
+	Prodotto(String nome, String marca, float prezzo){
+		
+		setCodice();
+		setNome(nome);
+		setMarca(marca);
+		setPrezzo(prezzo);
+		
+	}
+	
+	//GETTER ~ SETTER
+	
+	//CODICE PRODOTTO
+	public String getCodice() {
+		return codice;
+	}
+	private void setCodice() {
+		this.codice = "123ABC";
+	}
+
+	//NOME PRODOTTO
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//MARCA
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	//PREZZO
+	public float getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(float prezzo) {
+		
+		//logica per impostare il prezzo
+		
+		this.prezzo = prezzo;
+	}
+	
+	//IVA
+	public int getIva() {
+		return IVA;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return "Info Product: Codice : " + getCodice() + "|"
+			+ "Nome : " + getNome() + "|"
+			+ "Marca : " + getMarca() + "|"
+			+ "Prezzo : " + getPrezzo() + "|"
+			+ "IVA applicata : " + getIva()	+ "%"	
+			;
+	}
+	
+}
